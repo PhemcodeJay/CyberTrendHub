142837286bc3fef6850777749dc6bd8f3eae9c9c
PhemcodeJay <phemcodejay@gmail.com>
2024-11-04 16:45:34 +0300

    up

---
+++ DATABASE FILE/new.sql
@@ -0,0 +1,719 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 16, 2024 at 01:32 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerceweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_color`
+--
+
+CREATE TABLE `tbl_color` (
+  `color_id` int(11) NOT NULL,
+  `color_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_country`
+--
+
+CREATE TABLE `tbl_country` (
+  `country_id` int(11) NOT NULL,
+  `country_name` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customer`
+--
+
+CREATE TABLE `tbl_customer` (
+  `cust_id` int(11) NOT NULL,
+  `cust_name` varchar(100) NOT NULL,
+  `cust_cname` varchar(100) NOT NULL,
+  `cust_email` varchar(100) NOT NULL,
+  `cust_phone` varchar(50) NOT NULL,
+  `cust_country` int(11) NOT NULL,
+  `cust_address` text NOT NULL,
+  `cust_city` varchar(100) NOT NULL,
+  `cust_state` varchar(100) NOT NULL,
+  `cust_zip` varchar(30) NOT NULL,
+  `cust_b_name` varchar(100) NOT NULL,
+  `cust_b_cname` varchar(100) NOT NULL,
+  `cust_b_phone` varchar(50) NOT NULL,
+  `cust_b_country` int(11) NOT NULL,
+  `cust_b_address` text NOT NULL,
+  `cust_b_city` varchar(100) NOT NULL,
+  `cust_b_state` varchar(100) NOT NULL,
+  `cust_b_zip` varchar(30) NOT NULL,
+  `cust_s_name` varchar(100) NOT NULL,
+  `cust_s_cname` varchar(100) NOT NULL,
+  `cust_s_phone` varchar(50) NOT NULL,
+  `cust_s_country` int(11) NOT NULL,
+  `cust_s_address` text NOT NULL,
+  `cust_s_city` varchar(100) NOT NULL,
+  `cust_s_state` varchar(100) NOT NULL,
+  `cust_s_zip` varchar(30) NOT NULL,
+  `cust_password` varchar(100) NOT NULL,
+  `cust_token` varchar(255) NOT NULL,
+  `cust_datetime` varchar(100) NOT NULL,
+  `cust_timestamp` varchar(100) NOT NULL,
+  `cust_status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customer_message`
+--
+
+CREATE TABLE `tbl_customer_message` (
+  `customer_message_id` int(11) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `order_detail` text NOT NULL,
+  `cust_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_end_category`
+--
+
+CREATE TABLE `tbl_end_category` (
+  `ecat_id` int(11) NOT NULL,
+  `ecat_name` varchar(255) NOT NULL,
+  `mcat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_faq`
+--
+
+CREATE TABLE `tbl_faq` (
+  `faq_id` int(11) NOT NULL,
+  `faq_title` varchar(255) NOT NULL,
+  `faq_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_language`
+--
+
+CREATE TABLE `tbl_language` (
+  `lang_id` int(11) NOT NULL,
+  `lang_name` varchar(255) NOT NULL,
+  `lang_value` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mid_category`
+--
+
+CREATE TABLE `tbl_mid_category` (
+  `mcat_id` int(11) NOT NULL,
+  `mcat_name` varchar(255) NOT NULL,
+  `tcat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `size` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
+  `quantity` varchar(50) NOT NULL,
+  `unit_price` varchar(50) NOT NULL,
+  `payment_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_page`
+--
+
+CREATE TABLE `tbl_page` (
+  `id` int(11) NOT NULL,
+  `about_title` varchar(255) NOT NULL,
+  `about_content` text NOT NULL,
+  `about_banner` varchar(255) NOT NULL,
+  `about_meta_title` varchar(255) NOT NULL,
+  `about_meta_keyword` text NOT NULL,
+  `about_meta_description` text NOT NULL,
+  `faq_title` varchar(255) NOT NULL,
+  `faq_banner` varchar(255) NOT NULL,
+  `faq_meta_title` varchar(255) NOT NULL,
+  `faq_meta_keyword` text NOT NULL,
+  `faq_meta_description` text NOT NULL,
+  `blog_title` varchar(255) NOT NULL,
+  `blog_banner` varchar(255) NOT NULL,
+  `blog_meta_title` varchar(255) NOT NULL,
+  `blog_meta_keyword` text NOT NULL,
+  `blog_meta_description` text NOT NULL,
+  `contact_title` varchar(255) NOT NULL,
+  `contact_banner` varchar(255) NOT NULL,
+  `contact_meta_title` varchar(255) NOT NULL,
+  `contact_meta_keyword` text NOT NULL,
+  `contact_meta_description` text NOT NULL,
+  `pgallery_title` varchar(255) NOT NULL,
+  `pgallery_banner` varchar(255) NOT NULL,
+  `pgallery_meta_title` varchar(255) NOT NULL,
+  `pgallery_meta_keyword` text NOT NULL,
+  `pgallery_meta_description` text NOT NULL,
+  `vgallery_title` varchar(255) NOT NULL,
+  `vgallery_banner` varchar(255) NOT NULL,
+  `vgallery_meta_title` varchar(255) NOT NULL,
+  `vgallery_meta_keyword` text NOT NULL,
+  `vgallery_meta_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_payment`
+--
+
+CREATE TABLE `tbl_payment` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `customer_email` varchar(255) NOT NULL,
+  `payment_date` varchar(50) NOT NULL,
+  `txnid` varchar(255) NOT NULL,
+  `paid_amount` int(11) NOT NULL,
+  `card_number` varchar(50) NOT NULL,
+  `card_cvv` varchar(10) NOT NULL,
+  `card_month` varchar(10) NOT NULL,
+  `card_year` varchar(10) NOT NULL,
+  `bank_transaction_info` text NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `payment_status` varchar(25) NOT NULL,
+  `shipping_status` varchar(20) NOT NULL,
+  `payment_id` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_photo`
+--
+
+CREATE TABLE `tbl_photo` (
+  `id` int(11) NOT NULL,
+  `caption` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_post`
+--
+
+CREATE TABLE `tbl_post` (
+  `post_id` int(11) NOT NULL,
+  `post_title` varchar(255) NOT NULL,
+  `post_slug` varchar(255) NOT NULL,
+  `post_content` text NOT NULL,
+  `post_date` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `total_view` int(11) NOT NULL,
+  `meta_title` varchar(255) NOT NULL,
+  `meta_keyword` text NOT NULL,
+  `meta_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `p_id` int(11) NOT NULL,
+  `p_name` varchar(255) NOT NULL,
+  `p_old_price` varchar(10) NOT NULL,
+  `p_current_price` varchar(10) NOT NULL,
+  `p_qty` int(10) NOT NULL,
+  `p_featured_photo` varchar(255) NOT NULL,
+  `p_description` text NOT NULL,
+  `p_short_description` text NOT NULL,
+  `p_feature` text NOT NULL,
+  `p_condition` text NOT NULL,
+  `p_return_policy` text NOT NULL,
+  `p_total_view` int(11) NOT NULL,
+  `p_is_featured` int(1) NOT NULL,
+  `p_is_active` int(1) NOT NULL,
+  `ecat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product_color`
+--
+
+CREATE TABLE `tbl_product_color` (
+  `id` int(11) NOT NULL,
+  `color_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product_photo`
+--
+
+CREATE TABLE `tbl_product_photo` (
+  `pp_id` int(11) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `p_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product_size`
+--
+
+CREATE TABLE `tbl_product_size` (
+  `id` int(11) NOT NULL,
+  `size_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_rating`
+--
+
+CREATE TABLE `tbl_rating` (
+  `rt_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_service`
+--
+
+CREATE TABLE `tbl_service` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_settings`
+--
+
+CREATE TABLE `tbl_settings` (
+  `id` int(11) NOT NULL,
+  `logo` text NOT NULL,
+  `favicon` text NOT NULL,
+  `footer_about` text NOT NULL,
+  `footer_copyright` text NOT NULL,
+  `contact_address` text NOT NULL,
+  `contact_email` varchar(255) NOT NULL,
+  `contact_phone` varchar(255) NOT NULL,
+  `contact_fax` varchar(255) NOT NULL,
+  `contact_map_iframe` text NOT NULL,
+  `receive_email` varchar(255) NOT NULL,
+  `receive_email_subject` varchar(255) NOT NULL,
+  `receive_email_thank_you_message` text NOT NULL,
+  `forget_password_message` text NOT NULL,
+  `total_recent_post_footer` int(10) NOT NULL,
+  `total_popular_post_footer` int(10) NOT NULL,
+  `total_recent_post_sidebar` int(11) NOT NULL,
+  `total_popular_post_sidebar` int(11) NOT NULL,
+  `total_featured_product_home` int(11) NOT NULL,
+  `total_latest_product_home` int(11) NOT NULL,
+  `total_popular_product_home` int(11) NOT NULL,
+  `meta_title_home` text NOT NULL,
+  `meta_keyword_home` text NOT NULL,
+  `meta_description_home` text NOT NULL,
+  `banner_login` text NOT NULL,
+  `banner_registration` text NOT NULL,
+  `banner_forget_password` text NOT NULL,
+  `banner_reset_password` text NOT NULL,
+  `banner_search` text NOT NULL,
+  `banner_cart` text NOT NULL,
+  `banner_checkout` text NOT NULL,
+  `banner_product_category` text NOT NULL,
+  `banner_blog` text NOT NULL,
+  `cta_title` varchar(255) NOT NULL,
+  `cta_content` text NOT NULL,
+  `cta_read_more_text` varchar(255) NOT NULL,
+  `cta_read_more_url` varchar(255) NOT NULL,
+  `cta_photo` text NOT NULL,
+  `featured_product_title` varchar(255) NOT NULL,
+  `featured_product_subtitle` varchar(255) NOT NULL,
+  `latest_product_title` varchar(255) NOT NULL,
+  `latest_product_subtitle` varchar(255) NOT NULL,
+  `popular_product_title` varchar(255) NOT NULL,
+  `popular_product_subtitle` varchar(255) NOT NULL,
+  `testimonial_title` varchar(255) NOT NULL,
+  `testimonial_subtitle` varchar(255) NOT NULL,
+  `testimonial_photo` text NOT NULL,
+  `blog_title` varchar(255) NOT NULL,
+  `blog_subtitle` varchar(255) NOT NULL,
+  `newsletter_text` text NOT NULL,
+  `paypal_email` varchar(255) NOT NULL,
+  `stripe_public_key` text NOT NULL,
+  `stripe_secret_key` text NOT NULL,
+  `bank_detail` text NOT NULL,
+  `before_head` text NOT NULL,
+  `after_body` text NOT NULL,
+  `before_body` text NOT NULL,
+  `home_service_on_off` int(11) NOT NULL,
+  `home_welcome_on_off` int(11) NOT NULL,
+  `home_featured_product_on_off` int(11) NOT NULL,
+  `home_latest_product_on_off` int(11) NOT NULL,
+  `home_popular_product_on_off` int(11) NOT NULL,
+  `home_testimonial_on_off` int(11) NOT NULL,
+  `home_blog_on_off` int(11) NOT NULL,
+  `newsletter_on_off` int(11) NOT NULL,
+  `ads_above_welcome_on_off` int(1) NOT NULL,
+  `ads_above_featured_product_on_off` int(1) NOT NULL,
+  `ads_above_latest_product_on_off` int(1) NOT NULL,
+  `ads_above_popular_product_on_off` int(1) NOT NULL,
+  `ads_above_testimonial_on_off` int(1) NOT NULL,
+  `ads_category_sidebar_on_off` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_shipping_cost`
+--
+
+CREATE TABLE `tbl_shipping_cost` (
+  `shipping_cost_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `amount` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_shipping_cost_all`
+--
+
+CREATE TABLE `tbl_shipping_cost_all` (
+  `sca_id` int(11) NOT NULL,
+  `amount` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_size`
+--
+
+CREATE TABLE `tbl_size` (
+  `size_id` int(11) NOT NULL,
+  `size_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_slider`
+--
+
+CREATE TABLE `tbl_slider` (
+  `id` int(11) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `heading` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `button_text` varchar(255) NOT NULL,
+  `button_url` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_social`
+--
+
+CREATE TABLE `tbl_social` (
+  `social_id` int(11) NOT NULL,
+  `social_name` varchar(30) NOT NULL,
+  `social_url` varchar(255) NOT NULL,
+  `social_icon` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_subscriber`
+--
+
+CREATE TABLE `tbl_subscriber` (
+  `subs_id` int(11) NOT NULL,
+  `subs_email` varchar(255) NOT NULL,
+  `subs_date` varchar(100) NOT NULL,
+  `subs_date_time` varchar(100) NOT NULL,
+  `subs_hash` varchar(255) NOT NULL,
+  `subs_active` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_top_category`
+--
+
+CREATE TABLE `tbl_top_category` (
+  `tcat_id` int(11) NOT NULL,
+  `tcat_name` varchar(255) NOT NULL,
+  `show_on_menu` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(10) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `role` varchar(30) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_video`
+--
+
+CREATE TABLE `tbl_video` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `iframe_code` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_color`
+--
+ALTER TABLE `tbl_color`
+  ADD PRIMARY KEY (`color_id`);
+
+--
+-- Indexes for table `tbl_country`
+--
+ALTER TABLE `tbl_country`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- Indexes for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  ADD PRIMARY KEY (`cust_id`);
+
+--
+-- Indexes for table `tbl_customer_message`
+--
+ALTER TABLE `tbl_customer_message`
+  ADD PRIMARY KEY (`customer_message_id`);
+
+--
+-- Indexes for table `tbl_end_category`
+--
+ALTER TABLE `tbl_end_category`
+  ADD PRIMARY KEY (`ecat_id`);
+
+--
+-- Indexes for table `tbl_faq`
+--
+ALTER TABLE `tbl_faq`
+  ADD PRIMARY KEY (`faq_id`);
+
+--
+-- Indexes for table `tbl_language`
+--
+ALTER TABLE `tbl_language`
+  ADD PRIMARY KEY (`lang_id`);
+
+--
+-- Indexes for table `tbl_mid_category`
+--
+ALTER TABLE `tbl_mid_category`
+  ADD PRIMARY KEY (`mcat_id`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_page`
+--
+ALTER TABLE `tbl_page`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_payment`
+--
+ALTER TABLE `tbl_payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_photo`
+--
+ALTER TABLE `tbl_photo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `tbl_product_color`
+--
+ALTER TABLE `tbl_product_color`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_product_photo`
+--
+ALTER TABLE `tbl_product_photo`
+  ADD PRIMARY KEY (`pp_id`);
+
+--
+-- Indexes for table `tbl_product_size`
+--
+ALTER TABLE `tbl_product_size`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_rating`
+--
+ALTER TABLE `tbl_rating`
+  ADD PRIMARY KEY (`rt_id`);
+
+--
+-- Indexes for table `tbl_service`
+--
+ALTER TABLE `tbl_service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_settings`
+--
+ALTER TABLE `tbl_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_shipping_cost`
+--
+ALTER TABLE `tbl_shipping_cost`
+  ADD PRIMARY KEY (`shipping_cost_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_settings`
+--
+ALTER TABLE `tbl_settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
