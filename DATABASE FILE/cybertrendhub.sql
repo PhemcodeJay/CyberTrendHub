0212f1f3b32fe4dcf45f5838e99aab738812008c
PhemcodeJay <phemcodejay@gmail.com>
2024-11-04 18:24:16 +0300

    up

--- DATABASE FILE/cybertrendhub.sql
+++ DATABASE FILE/cybertrendhub.sql
@@ -1631,18 +1631,18 @@ CREATE TABLE `tbl_social` (
 --
 
 INSERT INTO `tbl_social` (`social_id`, `social_name`, `social_url`, `social_icon`) VALUES
-(1, 'Facebook', 'https://www.facebook.com/#', 'fa fa-facebook'),
-(2, 'Twitter', 'https://www.twitter.com/#', 'fa fa-twitter'),
+(1, 'Facebook', 'https://www.facebook.com/cybertrendhub', 'fa fa-facebook'),
+(2, 'Twitter', 'https://www.twitter.com/phemcodejay', 'fa fa-twitter'),
 (3, 'LinkedIn', '', 'fa fa-linkedin'),
 (4, 'Google Plus', '', 'fa fa-google-plus'),
 (5, 'Pinterest', '', 'fa fa-pinterest'),
-(6, 'YouTube', 'https://www.youtube.com/#', 'fa fa-youtube'),
-(7, 'Instagram', 'https://www.instagram.com/#', 'fa fa-instagram'),
+(6, 'YouTube', 'https://www.youtube.com/cybertrendhub', 'fa fa-youtube'),
+(7, 'Instagram', 'https://www.instagram.com/cybertrendhub', 'fa fa-instagram'),
 (8, 'Tumblr', '', 'fa fa-tumblr'),
 (9, 'Flickr', '', 'fa fa-flickr'),
 (10, 'Reddit', '', 'fa fa-reddit'),
 (11, 'Snapchat', '', 'fa fa-snapchat'),
-(12, 'WhatsApp', 'https://www.whatsapp.com/#', 'fa fa-whatsapp'),
+(12, 'WhatsApp', 'https://www.whatsapp.com/+254101674289', 'fa fa-whatsapp'),
 (13, 'Quora', '', 'fa fa-quora'),
 (14, 'StumbleUpon', '', 'fa fa-stumbleupon'),
 (15, 'Delicious', '', 'fa fa-delicious'),
