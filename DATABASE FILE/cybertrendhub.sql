afa521277312ea4ba170ae2bd0fd9b8b30d0537a
PhemcodeJay <phemcodejay@gmail.com>
2024-11-06 02:04:04 +0300

    up

--- DATABASE FILE/cybertrendhub.sql
+++ DATABASE FILE/cybertrendhub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2024 at 12:01 AM
+-- Generation Time: Nov 06, 2024 at 12:02 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
