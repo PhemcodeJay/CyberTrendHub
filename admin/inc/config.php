ea41a46a8b8c36d1a3fd9a6fd2494dfa0e766215
phemcodejay <phemcode@live.com>
2024-11-24 19:06:33 -0800

    up

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -9,7 +9,7 @@ date_default_timezone_set('America/Los_Angeles');
 $dbhost = 'localhost';
 
 // Database Name
-$dbname = 'cybertrendhub';
+$dbname = 'dbs13461311';
 
 // Database Username
 $dbuser = 'root';
